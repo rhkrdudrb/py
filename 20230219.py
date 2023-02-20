e0fcf68ab5a1419153ce3d08b283609d36344749
곽영규 <rhkrdudrb12@naver.com>
2023-02-20 23:58:56 +0900

    end 물어보기

--- 20230219.py
+++ 20230219.py
@@ -10,7 +10,23 @@
 
 # 두 수의 곱중 최댓값은 31 * 24 = 744 입니다.
 def solution(numbers)
-    answer = 0
-    print(max(numbers))
+    # max(numbers) 이거 안됨..?
+    answer = numbers.sort()[-1] #최고값
+    nummax = numbers.sort()[-2] #두번째값
+    return answer * nummax
+end
+# 문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+# 제한사항
+# 2 ≤ my_string 길이 ≤ 5
+# 2 ≤ n ≤ 10
+# "my_string"은 영어 대소문자로 이루어져 있습니다.
+# 입출력 예
+# my_string	n	result
+# "hello"	3	"hhheeellllllooo"
+def solution(my_string, n)
+    answer = ''
+    print(my_string)
+    while len(my_string) >0:
     return answer
 end
