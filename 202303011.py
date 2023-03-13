9231847d36433acb616a2e82c170f1766409cb87
곽영규 <rhkrdudrb12@naver.com>
2023-03-13 16:44:42 +0900

    수업

---
+++ 202303011.py
@@ -0,0 +1,80 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/155652
+# 두 문자열 s와 skip, 그리고 자연수 index가 주어질 때, 다음 규칙에 따라 문자열을 만들려 합니다. 암호의 규칙은 다음과 같습니다.
+
+# 문자열 s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꿔줍니다.
+# index만큼의 뒤의 알파벳이 z를 넘어갈 경우 다시 a로 돌아갑니다.
+# skip에 있는 알파벳은 제외하고 건너뜁니다.
+# 예를 들어 s = "akuks", skip = "wbqd", index = 5일 때, a에서 5만큼 뒤에 있는 알파벳은 
+# f지만 [b, c, d, e, f]에서 'b'와 'd'는 skip에 포함되므로 세지 않습니다. 따라서 'b', 'd'를 제외하고 'a'에서 
+# 5만큼 뒤에 있는 알파벳은 [c, e, f, g, h] 순서에 의해 'h'가 됩니다. 나머지 "ukks" 또한 위 규칙대로 바꾸면 "appy"가 되며
+#  결과는 "happy"가 됩니다.
+
+# 두 문자열 s와 skip, 그리고 자연수 index가 매개변수로 주어질 때 위 규칙대로 s를 변환한 결과를 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 5 ≤ s의 길이 ≤ 50
+# 1 ≤ skip의 길이 ≤ 10
+# s와 skip은 알파벳 소문자로만 이루어져 있습니다.
+# skip에 포함되는 알파벳은 s에 포함되지 않습니다.
+# 1 ≤ index ≤ 20
+# 입출력 예
+# s	skip	index	result
+# "aukks"	"wbqd"	5	"happy"
+# 입출력 예 설명
+# 입출력 예 #1
+# 본문 내용과 일치합니다.
+
+# ord: ord('a') -> 67
+# chr: chr(67) -> 'a'
+
+def solution(s, skip, index):
+    # 1. ord 활용해서 a -> bcdef
+    answer = ''
+    ss =''
+    for letter in s:
+        ss = letter
+        answer += oneletter(ss, skip, index)
+    return answer
+    # 2. bcdef, skip이랑 비교해서 개수 dup를 구해서 
+    # 3. bcdef -> cef, replace 쓰지말기
+    # dup만큼 1번 다시
+print(solution('aukks', 'wbqd', 5)) #> 'h')
+# print(solution("aukks","wbqdgi",5))
+# a, "bcdef" -> "cef" -> "cefgh" -> "cefhi" -> "h"
+# u, 
+def oneletter(letter, skip, index): # 'a'
+    answer =''
+    while len(answer) != 5:
+        cnt = 0
+        for i in range(index):
+            answer += chr(ord(letter) + i + 1)
+        tmp = ''
+        for letter in answer:
+            if letter in skip:
+                cnt += 1
+            else:
+                tmp += letter
+        answer = tmp
+        print(answer)
+        for i in range(cnt):
+            answer += chr(ord(letter) +i +1) 
+        temp = '' 
+        cnt2 = 0 
+        print(answer)    
+        break
+    for letter in answer:
+        if letter in skip:
+            temp += letter
+            cnt2 += 1
+    print(answer)        
+    if cnt2 >= 1:
+        result = ''
+        for i in range(cnt2):
+            result += chr(ord(letter) +i +1)   
+        return answer[-1]    
+    else:
+        return answer[-1]       
+# print(oneletter('a', 'wbqd', 5)) #> 'h')
+        
+    
+        
