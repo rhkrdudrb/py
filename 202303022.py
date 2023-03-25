e105e67fb1cf451b10c3ec06aeecaf45551ac92a
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-03-25 14:17:14 +0900

    20230325 가독성

--- 202303022.py
+++ 202303022.py
@@ -1,15 +1,22 @@
 def solution(today, terms, privacies):
     answer = []
-    today = 12*28*int(today[:4]) +int(today[5:7])*28 +int(today[8:11]) #날짜를 일수로
-    terms = {i[:1]:int(i[2:])*28 for i in terms} #딕셔너리로
-    # privacies={i[-1]:12*28*int(i[:4]) +int(i[5:7])*28 +int(i[8:11]) for i in privacies} 중복떄문에 쓸수없음
-    num= 0
-    for letter in privacies:
-        num += 1
-        for key,value in terms.items():
-            if letter[-1] == key:
-                endday = 12*28*int(letter[:4]) +int(letter[5:7])*28 +int(letter[8:11])
-                if endday + value <= today:
-                    answer.append(num)
+    today = string2day(today)#날짜를 일수로
+    term2day = {}
+    for term in terms:
+        term, day = term.split()
+        term2day[term] = int(day)*28
+    # term2day = {i[:1]:int(i[2:])*28 for i in terms} #딕셔너리로
+    for num, privacy in enumerate(privacies, 1): # "2021.05.02 A" 
+        day, term = privacy.split()
+        startday = string2day(day)
+        if startday + term2day[term] <= today:
+            answer.append(num)
     return answer
-print(solution("2022.05.19",["A 6", "B 12", "C 3"],["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])) #[1, 3]
+
+# key -> value
+# key2value
+
+def string2day(s):
+    year, month, day = s.split(".") # (2022,05,19)
+    return 2*28*int(year) +int(month)*28 +int(day)
+print(solution("2022.05.19",["A 6", "B 12", "C 3"],["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])) #[1, 3] 
