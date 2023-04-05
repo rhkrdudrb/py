ad89970437e2dfb89ebca1c2a7cf50919b84528a
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-04-06 08:38:18 +0900

    20230406 테스트케이스 다 통과 돌려보면 3~12 안됨

--- 20230403햄버거만들기.py
+++ 20230403햄버거만들기.py
@@ -1,33 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/133502
 def solution(ingredient):
     answer = 0
-    arry =[]
-
-    for i in range(0,len(ingredient) - 4 +1):
-        arry.append(ingredient[i:4+i])
-        print(arry[i])
-        if arry[i] == [1,2,3,1]:
-            
-            # print(arry)
-            answer += 1   
-#     def solution(ingredient):
-#     answer = 0
-#     y=0
-#     for i in range(y,len(ingredient)):
-#         print(ingredient[i:4+i])
-#         if ingredient[i:4+i] == [1,2,3,1]:
-#             del ingredient[i:4+i]
-#             print(ingredient[i:4+i])
-#             print(i)
-#             print(len(ingredient))
-#             answer += 1 
-#     return answer
-    # 2 2 1 2
-    # 2 1 2 3
-    # 1 2 3 1
-    # 2 3 1 2
-    # 3 1 2 3
-    # 1 2 3 1
-    
+    y=0
+    for _ in range(len(ingredient)):
+        if not ingredient[y:4+y] or len(ingredient[y:4+y]) !=4:     
+            break
+        if ingredient[y:4+y] == [1,2,3,1]:
+            del ingredient[y:4+y]
+            y = 0
+            answer += 1 
+            continue
+        y+=1    
     return answer
 print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
