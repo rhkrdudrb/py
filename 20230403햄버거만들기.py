9e9b94e107fc8bb0a5355fecbedc53451eee49ad
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-04-06 14:19:54 +0900

    20230406 for문 while로 바꿈

--- 20230403햄버거만들기.py
+++ 20230403햄버거만들기.py
@@ -1,10 +1,11 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/133502
 def solution(ingredient):
     answer = 0
     y=0
-    for _ in range(len(ingredient)):
+    # for _ in range(len(ingredient)):
+    while True:    
         if not ingredient[y:4+y] or len(ingredient[y:4+y]) !=4:     
             break
+        # print(ingredient[y:4+y])
         if ingredient[y:4+y] == [1,2,3,1]:
             del ingredient[y:4+y]
             y = 0
@@ -12,4 +13,3 @@ def solution(ingredient):
             continue
         y+=1    
     return answer
-print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
