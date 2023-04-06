e37b9e62422fd9b2b71aefaa73dd38d9f475bf5a
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-04-07 08:16:32 +0900

    20230407 흠..

--- 20230403햄버거만들기.py
+++ 20230403햄버거만들기.py
@@ -1,15 +1,29 @@
+# def solution(ingredient):
+#     answer = 0
+#     y=0
+#     while True:    
+#         if not ingredient[y:4+y] or len(ingredient[y:4+y]) !=4:     
+#             break
+#         # print(ingredient[y:4+y])
+#         if ingredient[y:4+y] == [1,2,3,1]:
+#             del ingredient[y:4+y]
+#             y = 0
+#             answer += 1 
+#             continue
+#         y+=1   
 def solution(ingredient):
     answer = 0
     y=0
-    # for _ in range(len(ingredient)):
+    lastOne = []
     while True:    
-        if not ingredient[y:4+y] or len(ingredient[y:4+y]) !=4:     
+        lastOne = ingredient[y:4+y]
+        if not lastOne or len(lastOne) !=4:     
             break
-        # print(ingredient[y:4+y])
-        if ingredient[y:4+y] == [1,2,3,1]:
-            del ingredient[y:4+y]
-            y = 0
-            answer += 1 
-            continue
+        if lastOne[-1] == 1:
+            if lastOne == [1,2,3,1]:
+                del ingredient[y:4+y]
+                y = 0
+                answer += 1 
+                continue
         y+=1    
     return answer
