707c0221eccd61d5c8bd896cf7219323cfe0711d
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-04-09 09:20:21 +0900

    20230408 수업

--- 20230403햄버거만들기.py
+++ 20230403햄버거만들기.py
@@ -1,29 +1,35 @@
-# def solution(ingredient):
-#     answer = 0
-#     y=0
-#     while True:    
-#         if not ingredient[y:4+y] or len(ingredient[y:4+y]) !=4:     
-#             break
-#         # print(ingredient[y:4+y])
-#         if ingredient[y:4+y] == [1,2,3,1]:
-#             del ingredient[y:4+y]
-#             y = 0
-#             answer += 1 
-#             continue
-#         y+=1   
 def solution(ingredient):
     answer = 0
     y=0
-    lastOne = []
-    while True:    
-        lastOne = ingredient[y:4+y]
-        if not lastOne or len(lastOne) !=4:     
-            break
-        if lastOne[-1] == 1:
-            if lastOne == [1,2,3,1]:
-                del ingredient[y:4+y]
-                y = 0
-                answer += 1 
-                continue
-        y+=1    
+    stack = [] # 1, 2, 3, 1
+    for el in ingredient:
+        stack.append(el)
+        if el == 1 and len(stack) >= 4:
+            if stack[-4:] == [1,2,3,1]:
+                answer += 1
+                for _ in range(4):
+                    stack.pop()
+
     return answer
+# def solution(ingredient):
+#     answer = 0
+#     y=0
+#     lastOne = []
+#     while True:    
+#         lastOne = ingredient[y:4+y]
+#         if not lastOne or len(lastOne) !=4:     
+#             break
+#         if lastOne[-1] == 1:
+#             if lastOne == [1,2,3,1]:
+#                 del ingredient[y:4+y]
+#                 y = 0
+#                 answer += 1 
+#                 continue
+#         y+=1    
+#     return answer
+# stack
+
+12 1231 31
+
+
+
