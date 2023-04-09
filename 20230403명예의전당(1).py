cd13a7783884dd53b2884c88fe624b5b7b1331cf
곽영규 <rhkrdudrb12@naver.com>
2023-04-09 09:17:41 +0900

    20230409
    Co-authored-by: hyejin lee <geniecs@github.com>

--- 20230403명예의전당(1).py
+++ 20230403명예의전당(1).py
@@ -1,17 +1,24 @@
+from heapq import heappop, heappush
+
 # https://school.programmers.co.kr/learn/courses/30/lessons/138477
 def solution(k, score):
     answer = []
-    hall=[]     #명예의 전당
+    heap=[]     #명예의 전당
     for num in score:
-        if len(hall) == k: 
-            if num >= min(hall): #가장 작은값 삭제 및 num 삽입 
-                hall.remove(min(hall))
-                hall.append(num)
-                answer.append(min(hall))
-            else: #num이 명예의 전당 가장작은 값보다 작으므로 answer 작은값 삽입
-                answer.append(min(hall))
-        else : # k길이까지 명예의 전당 배열 append
-            hall.append(num)
-            answer.append(min(hall))
+        if len(heap) == k and heap[0] < num:
+            heappop(heap) #가장작은값 빠짐
+        if len(heap) < k:
+            heappush(heap, num)
+        answer.append(heap[0])
     return answer
+
+
 print(solution(3,[10, 100, 20, 150, 1, 100, 200]))
+
+# min(a, b)
+# max(a, b)
+
+[10, 20, 45, 60, 70, 80, 90, 100] , 85
+
+# min-heap, max-heap
+# 첫번쨰애가 제일작음 가장작은 애만 궁금할떄 정렬안되어도 상관없음
