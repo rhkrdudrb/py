4a04d9c9158acb3b49316511b40ca07cfa0b0c43
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-04-10 09:37:42 +0900

    20230410 시간 초과...

--- 20230408덧칠하기.py
+++ 20230408덧칠하기.py
@@ -1,21 +1,38 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/161989
+# https://school.programmers.co.kr/learn/courses/30/lessons/161989
 def solution(n, m, section):
     answer = 0
-    arry,answer = meter(m,section[0],answer)
-    while True:
-        if arry[-1] > section[-1]:
-            break
-        else:
-            arry,answer = meter(m,arry[-1] + 1,answer)
-            if arry[-1] == section[-1]:
-                break
+    y = 0
+    arry = []
+    while len(section) > 0:
+        for i in range(m):
+            arry.append(section[y] + y +i)
+        delete = list(set(arry) & set(section))
+        for dnum in delete:
+            section.remove(dnum)
+        answer += 1
     return answer
-def meter(m,num,answer):
-    arry =[]
-    for i in range(m):
-        arry.append(num+i)
-    answer +=1
-    return arry,answer
+# print(solution(8,4,[2,3,6]))
+# print(solution(5,4,[1,3]))
+print(solution(4,1,[1,2,3,4]))
+# 8, 4
+# 2,3,6
+# 2 3 4 5 6
+# X X X X
+#         X X X X 
+
+# 5, 4
+# 1, 3
+# 1 2 3 4 5
+# X X X X 
+
+# 4 ,1
+# 1 2 3 4
+# 1 2 3 4 
+# X
+#   X
+#     X
+#       X
 
 # print(solution(8,4,[2,3,6]))
 # print(solution(5,4,[1,3]))
