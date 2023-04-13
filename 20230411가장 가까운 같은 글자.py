eecbf8307715a8175ea85b8422e5dee6234a7342
곽영규 <rhkrdudrb12@naver.com>
2023-04-13 20:35:00 +0900

    20230413

--- 20230411가장 가까운 같은 글자.py
+++ 20230411가장 가까운 같은 글자.py
@@ -1,14 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/142086
 def solution(s):
     answer = []
-    arry =list(set(s))
+    result = []
+    for letter in s:
+        if letter not in result:
+            result.append(letter)
     y = 0
-    print(arry)
+    key = []
     while len(s) != len(answer):
         for i in range(len(s)):
-            if s[i] == arry[y]:
+            if s[i] == result[y]:
+                key.append(result[y])
                 answer.append(i)  
         y+=1
-        print(answer)
+    print(key)
+    print(answer)
     return answer
 print(solution("banana"))
