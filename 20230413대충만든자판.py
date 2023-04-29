ff82a2554082c0971b3b13fa90253c06bc93b0bd
곽영규 <rhkrdudrb12@naver.com>
2023-04-29 14:01:02 +0900

    20230429 수업 자판 코드개선 및 문자열나누기 문제 풀어오기
    Co-authored-by: hyejin lee <geniecs@github.com>

--- 20230413대충만든자판.py
+++ 20230413대충만든자판.py
@@ -1,25 +1,39 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/160586
+from collections import defaultdict
 def solution(keymap, targets):
     answer = []
+    letter2cnt = create_letter2cnt(keymap)
     for target in targets: # targets: ["ABCD", "AABB"], target: "ABCD"
         result = 0
-        for keym in target: # keym:"A","B","C","D"
-            min = float('inf') #무한대 
-            for ss in keymap: # ss: "ABACD", keymap: ["ABACD", "BCEFD"]
-                if min > ss.find(keym) and ss.find(keym) != -1:
-                    min = ss.find(keym) +1
-            result += min
-        if result == float('inf'):
-            answer.append(-1)
-        else:
-            answer.append(result)
+        for letter in target: # keym:"A","B","C","D"
+            if letter not in letter2cnt:
+                result = -1
+                break
+            result += letter2cnt[letter] # 1 + 1 + 2 + 5
+        answer.append(result)
     return answer
 print(solution(["ABACD", "BCEFD"],["ABCD", "AABB"])) # [9,4]
 print(solution(["AA"], ["B"])) # [-1]	
 print(solution(["AGZ", "BSSS"], ["ASA", "BGZ"])) # [4,6]
 	
+
+def create_letter2cnt(keymap): # ["ABACD", "BCEFD"]
+    letter2cnt = {}
+    for word in keymap: # ABACD, BCEFD
+        for i,letter in enumerate(word,1): # A, B, A, C, D
+            if letter in letter2cnt: #키가 딕셔너리에 있냐? (문법)
+                if letter2cnt[letter] > i :
+                    letter2cnt[letter] = i
+            else: 
+                letter2cnt[letter] = i #A -> 1, B->1, C->2 ,D-> 5 | E-> 3, F->4 
+    return letter2cnt
+# A -> 1
+# B -> 1
+# C -> 2
+# D -> 5
+
 # keymap
-# ["ABACD", "BCEFD"]	["ABCD"]
+# ["ABACD", "BCEFD"]	["DDDDD"]
 # A -> 1
 # B -> 1
 # C -> 2
---
+++ 20230429문자열나누기.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/140108
+
+def solution(s):
+    # 첫글자 구하고(x) x와 x아닌문자 길이 구하기
+    x = s[0]
+    answer = 0
+    x_cnt = 0
+    other_cnt = 0
+    
+    return answer
+
+# banana -> 3
+# ba - na - na
+# abracadabra -> 6
+# ab - ra - ca - da - br - a
+# aaabbacc - ccabba -> 3
+# 
