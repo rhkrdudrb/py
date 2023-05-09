11c4efcb4a40f73c243226bee3194f5d50e47d9b
곽영규 <rhkrdudrb12@naver.com>
2023-05-09 22:57:42 +0900

    20230509 추억점수 시작

---
+++ 20230509 추억점수.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/176963
+
+def solution(name, yearning, photo):
+    answer = []
+    return answer
+print(solution(["may", "kein", "kain", "radi"],[5, 10, 1, 3],[["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]))
+print(solution(["kali", "mari", "don"],[11, 1, 55],	[["kali", "mari", "don"], ["pony", "tom", "teddy"], ["con", "mona", "don"]]))
+print(solution(["may", "kein", "kain", "radi"],[5, 10, 1, 3],[["may"],["kein", "deny", "may"], ["kon", "coni"]]))
+    
