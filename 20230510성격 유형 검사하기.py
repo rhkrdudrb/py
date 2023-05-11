2cf401ab6b8ee910faae8f563f0efea29b6b9b2b
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-05-12 08:41:54 +0900

    20230512 성격유형검사 완료

--- 20230510성격 유형 검사하기.py
+++ 20230510성격 유형 검사하기.py
@@ -1,9 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118666
-
 def solution(survey, choices):
     answer = ''
-    table = {"A":0,"C":0,"F":0,"J":0,"M":0,"N":0,"R":0,"T":0}    
-    score = {1:3,2:2,3:1,4:0,5:1,6:2,7:3}
+    table = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    score = {1 : 3, 2 : 2, 3 : 1, 4 : 0, 5 : 1, 6 : 2, 7 : 3}
+    # 질문의 성격 유형점수 합한값
     for i in range(len(choices)):
         if choices[i] in score:
             if choices[i] <= 4:
@@ -12,13 +12,16 @@ def solution(survey, choices):
             else:
                 if survey[i][1] in table:
                     table[survey[i][1]] += score[choices[i]]
+    # 출력 
+    table_key = list(table.keys())
     
-    min_value = min(table.values())
-    max_value = max(table.values())
-    print(dict(sorted(table.items(), key=lambda x: x[1])))
-    # value 값으로 정렬후 min값이 0이면 
-    # while len(answer) !=4:
-    sorted(table.items(), key=lambda x: x[1])
+    for i in range(0, len(table), 2):
+        if table[table_key[i]] > table[table_key[i+1]]:
+            answer += table_key[i]
+        elif table[table_key[i]] < table[table_key[i+1]]:
+            answer += table_key[i+1]
+        else:
+            answer += min(table_key[i], table_key[i+1])
     return answer
 print(solution(["AN", "CF", "MJ", "RT", "NA"],[5, 3, 2, 7, 5]))
 print(solution(["TR", "RT", "TR"],[7, 1, 3]))
