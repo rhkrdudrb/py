76faa2c6cd9c473cc303ac86e260512b900581f7
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-05-19 09:04:14 +0900

    20230519 드디어 성공!

--- 20230515다트게임.py
+++ 20230515다트게임.py
@@ -1,32 +1,57 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/17682
 def solution(dartResult):
-    score = ''
+    dartResult = dartResult.replace("10","A")
     answer=[]
+    for i in range(len(dartResult)):
+        if dartResult[i] =='S':
+            if dartResult[i-1] =='A':
+                answer.append(10)
+            else:
+                answer.append(int(dartResult[i-1]))
+        elif dartResult[i] =='D':
+            if dartResult[i-1] =='A':
+                answer.append(10**2)
+            else:
+                answer.append(int(dartResult[i-1])**2)
+        elif dartResult[i] =='T':
+            if dartResult[i-1] =='A':
+                answer.append(10**3)
+            else:
+                answer.append(int(dartResult[i-1])**3)
+        elif dartResult[i] =='*':
+            answer[-1] *= 2
+            if len(answer) >= 2:
+                answer[-2] *= 2
+        elif dartResult[i] =='#':
+            answer[-1] *= -1
+    print(answer)
     # 문자열 분리
-    for i,letter in enumerate(dartResult):
-        if letter not in '*#':
-            score+=letter
-    j=0
-    # S,D,T 제곱
-    print(score)
-    for i in range(len(score)): 
-        if score[i] =='S':
-            answer.append(int(score[j:i]))
-            j = i +1
-        elif score[i] =='D':
-            answer.append(int(score[j:i])**2)
-            j = i +1
-        elif score[i] =='T':
-            answer.append(int(score[j:i])**3)
-            j = i +1
-    # 스타상 아차상 계산   
-    for i,letter in enumerate(dartResult):
-        if letter == '#':
-            answer[int(i/2 -1)] *= -1
-        elif letter == '*':
-            answer[int(i/2 -1)] *= 2
-            if 0 <= int(i/2 -1)-1:
-                answer[int(i/2 -1)-1] *= 2
+#     answer=[]
+#     score
+    # for i,letter in enumerate(dartResult):
+    #     if letter not in '*#':
+    #         score+=letter
+    # j=0
+    # # S,D,T 제곱
+    # print(score)
+    # for i in range(len(score)): 
+    #     if score[i] =='S':
+    #         answer.append(int(score[j:i]))
+    #         j = i +1
+    #     elif score[i] =='D':
+    #         answer.append(int(score[j:i])**2)
+    #         j = i +1
+    #     elif score[i] =='T':
+    #         answer.append(int(score[j:i])**3)
+    #         j = i +1
+    # # 스타상 아차상 계산   
+    # for i,letter in enumerate(dartResult):
+    #     if letter == '#':
+    #         answer[int(i/2 -1)] *= -1
+    #     elif letter == '*':
+    #         answer[int(i/2 -1)] *= 2
+    #         if 0 <= int(i/2 -1)-1:
+    #             answer[int(i/2 -1)-1] *= 2
     return sum(answer)
 print(solution("1S2D*3T")) #37	11 * 2 + 22 * 2 + 33
 print(solution("1D2S#10S"))#9	12 + 21 * (-1) + 101
