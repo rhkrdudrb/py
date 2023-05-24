72856aec01e20e143f56979624053a164b3f46c4
곽영규 <rhkrdudrb12@naver.com>
2023-05-24 19:12:14 +0900

    20230524 숫자변환하기 왜 안되는거임??

---
+++ (LV2)20230524 숫자변환하기.py
@@ -0,0 +1,46 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154538
+def solution(x, y, n):
+    answer = 0
+    seq1 = step1(x,y,n)
+    seq2 = step2(x,y)
+    seq3 = step3(x,y)
+    if seq1 == float("inf") and seq2 == float("inf") and seq3 == float("inf"):
+        return -1
+    return min(seq1,seq2,seq3)
+def step1 (x_num,y_num,n_num):
+    answer = 0
+    cnt = 0
+    while y_num > answer:
+        answer += x_num+n_num
+        cnt += 1
+    if answer > y_num:
+        return float("inf")
+    return cnt
+def step2 (x_num,y_num):
+    answer = 0
+    cnt = 0
+    while y_num > answer:
+        answer += x_num*2
+        cnt += 1
+    if answer > y_num:
+        return float("inf")
+    return cnt
+def step3 (x_num,y_num):
+    answer = 0
+    cnt = 0
+    while y_num > answer:
+        answer += x_num*3
+        cnt += 1
+    if answer > y_num:
+        return float("inf")
+    return cnt
+print(solution(10,40,5))   # 2
+print(solution(10,40,30))  # 1
+print(solution(2,5,4))     # -1
+
+
+
+
+
+
+
