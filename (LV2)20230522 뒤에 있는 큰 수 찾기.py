a878f8fbc53451c26b1f4d317614d5016f0e7cab
곽영규 <rhkrdudrb12@naver.com>
2023-05-27 14:02:40 +0900

    20230527 수업 어려움..

--- (LV2)20230522 뒤에 있는 큰 수 찾기.py
+++ (LV2)20230522 뒤에 있는 큰 수 찾기.py
@@ -1,21 +1,32 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/154539
 def solution(numbers):
-    for i in range(len(numbers)-1):
-        changed = True
-        for j in range(i+1,len(numbers)):
-            if numbers[i] < numbers[j] :
-                numbers[i] = numbers[j]
-                changed = False
-                break
-        if changed :
-            numbers[i] = -1
-    numbers[-1] = -1
-    return numbers
+    nums = []
+    answer = []
+    for num in numbers[::-1]:
+        # if len(nums) == 0:
+        #     answer.append(-1)
+        # elif num < nums[-1]:
+        #     answer.append(nums[-1])
+            
+        while nums and nums[-1] <= num:
+            nums.pop()
+        if len(nums) == 0:
+            answer.append(-1)
+        else:
+            answer.append(nums[-1])
+        nums.append(num)
+    return answer[::-1]
 print(solution([2, 3, 3, 5]))       # [3, 5, 5, -1]
 print(solution([9, 1, 5, 3, 6, 2])) # [-1, 5, 6, 6, -1, -1]
 
 
+[2,3,3,5]
+
+nums: [5,3,2]
+answer: [-1,5,5,3]
 
 
+[9, 1, 5, 3, 6, 2]
 
-
+nums: [] 
+answer: [-1,-1,6,6,5,-1]
