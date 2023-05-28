802e8a8f83d5ad7066354b1f69449f3acc45392b
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-05-28 21:48:22 +0900

    20230528 ...?

--- (LV2)20230524 숫자변환하기.py
+++ (LV2)20230524 숫자변환하기.py
@@ -4,39 +4,44 @@ def solution(x, y, n):
     seq1 = step1(x,y,n)
     seq2 = step2(x,y)
     seq3 = step3(x,y)
-    if seq1 == float("inf") and seq2 == float("inf") and seq3 == float("inf"):
+    print(seq1)
+    print(seq2)
+    print(seq3)
+    if seq1[1] == y or seq2[1] == y or seq3[1] == y :
+        return min(seq1[0],seq2[0],seq3[0])
+    else:
         return -1
-    return min(seq1,seq2,seq3)
 def step1 (x_num,y_num,n_num):
-    answer = 0
+    answer = x_num
     cnt = 0
     while y_num > answer:
-        answer += x_num+n_num
+        answer += n_num
         cnt += 1
-    print('일번',answer)
-    if answer != y_num:
-        return float("inf")
-    return cnt
+    return cnt,answer
+
 def step2 (x_num,y_num):
-    answer = 0
+    answer = x_num
     cnt = 0
     while y_num > answer:
-        answer += x_num*2
+        answer *=2
         cnt += 1
-    print('이번',answer)
-    if answer != y_num:
-        return float("inf")
-    return cnt
+    return cnt,answer
+
 def step3 (x_num,y_num):
-    answer = 0
+    answer =x_num
     cnt = 0
     while y_num > answer:
-        answer += x_num*3
+        answer *=3
         cnt += 1
-    print('삼번',answer)
-    if answer != y_num:
-        return float("inf")
-    return cnt
+    
+    return cnt,answer
+
+# x 10  y 60  n 5
+
+# 10 15 20 25 30 35 40 45 50 55 60 -->10
+# 10 20 40 60 -> 3
+# 10 30 60 -> 2
+
 print(solution(10,40,5))   # 2
 print(solution(10,40,30))  # 1
 print(solution(2,5,4))     # -1
