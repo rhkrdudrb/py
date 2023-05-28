fe034732fabd74a3c594341c28e132d7a5d3aa25
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-05-29 00:43:19 +0900

    Update (LV2)20230524 숫자변환하기.py

--- (LV2)20230524 숫자변환하기.py
+++ (LV2)20230524 숫자변환하기.py
@@ -1,16 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/154538
 def solution(x, y, n):
     answer = 0
-    seq1 = step1(x,y,n)
-    seq2 = step2(x,y)
-    seq3 = step3(x,y)
-    print(seq1)
-    print(seq2)
-    print(seq3)
-    if seq1[1] == y or seq2[1] == y or seq3[1] == y :
-        return min(seq1[0],seq2[0],seq3[0])
-    else:
+    seq1 = list(step1(x,y,n))
+    seq2 = list(step2(x,y))
+    seq3 = list(step3(x,y))
+    if seq1[1] != y:
+        seq1[0] = float("inf")
+    if seq2[1] != y:
+        seq2[0] = float("inf")
+    if seq3[1] != y:
+        seq3[0] = float("inf")    
+    if seq1[0] == float("inf") and seq2[0] == float("inf") and seq3[0] == float("inf") :
         return -1
+    return min(seq1[0],seq2[0],seq3[0])
 def step1 (x_num,y_num,n_num):
     answer = x_num
     cnt = 0
@@ -33,14 +35,7 @@ def step3 (x_num,y_num):
     while y_num > answer:
         answer *=3
         cnt += 1
-    
     return cnt,answer
-
-# x 10  y 60  n 5
-
-# 10 15 20 25 30 35 40 45 50 55 60 -->10
-# 10 20 40 60 -> 3
-# 10 30 60 -> 2
 
 print(solution(10,40,5))   # 2
 print(solution(10,40,30))  # 1
