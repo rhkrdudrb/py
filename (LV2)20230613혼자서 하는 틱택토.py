b0fe3e6e87f8a9b0d64ff829639d777ab7dbcf0d
곽영규 <rhkrdudrb12@naver.com>
2023-06-13 22:25:25 +0900

    20230613 주석 추가 물어보기

--- (LV2)20230613혼자서 하는 틱택토.py
+++ (LV2)20230613혼자서 하는 틱택토.py
@@ -9,18 +9,18 @@ def solution(board):
                 o_cnt += 1
             if board[i][j] =='X':
                 x_cnt += 1
-    if o_cnt < x_cnt: #실수할때
+    if o_cnt < x_cnt: #실수할때(선공이 없는데 후공이 먼저할때)
         answer = 0
-    if o_cnt == 0 and x_cnt == 0 : 
+    if o_cnt == 0 and x_cnt == 0 : #선공 후공 아무도 공격하지 않을때
         return  1
     for i in range(3):
-        if board[i][0] == board[i][1] and board[i][1] == board[i][2]: #세로
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2]: #세로 같은때
             answer = 0
-        if board[0][i] == board[1][i] and board[1][i] == board[2][i]: #가로
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]: #가로 같을때
             answer = 0
-    if board[0][0] == board[1][1] and board[0][0] == board[2][2]: #대각선(\)
+    if board[0][0] == board[1][1] and board[0][0] == board[2][2]: #대각선(\) 같을때
         answer = 0
-    if board[0][2] == board[1][1] and board[0][2] == board[2][0]: #대각선(/)
+    if board[0][2] == board[1][1] and board[0][2] == board[2][0]: #대각선(/) 같을때
         answer = 0 
     return answer
 print(solution(["O.X", ".O.", "..X"]))
