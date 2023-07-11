5665dd41e26f847337964c57a93b37e6e0b4e17b
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-07-11 10:12:13 +0900

    MAX함수안씀

--- (LV2)프로세스.py
+++ (LV2)프로세스.py
@@ -20,10 +20,30 @@ def solution(priorities, location):
 print(solution([1, 1, 9, 1, 1, 1],0))
 # list - append, pop : 오른쪽에서 넣고 빼고 가능
 # deque - append, appendleft, pop, popleft : 양쪽에서 넣고 뺴고 가능
-
-
     # [2, 1, 3, 2], 2 >> 1
     # [(1), 1, 9, 1, 1, 1], 0 >> 
     # [1, 9, 1, 1, 1, (1)]
     # [9, 1, 1, 1, (1), 1]
     # [1, 1, 1, (1), 1], 1
+#------------------------------------------------------------------
+#MAX함수 안씀
+from collections import deque
+def solution(priorities, location):
+    answer = 0
+    queue = deque(priorities)
+    check = deque([0 for _ in range(len(priorities))])
+    check[location] = 1
+    while queue:
+        priorities = queue.popleft()
+        checkpop = check.popleft()
+        bignum = 0
+        for num in queue:
+             if num > bignum:
+                bignum = num
+        if queue and priorities < bignum :
+            queue.append(priorities)
+            check.append(checkpop)
+        else :
+            answer+=1   
+            if checkpop == 1:
+                return answer
