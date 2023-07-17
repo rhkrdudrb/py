019f9caae8db04838bf7eeceeed1ab5dfe654213
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-07-17 16:01:29 +0900

    많이틀림 이유가..?

--- (LV2)디펜스게임.py
+++ (LV2)디펜스게임.py
@@ -1,24 +1,47 @@
 from heapq import heappush, heappop, heapify
-
 # https://school.programmers.co.kr/learn/courses/30/lessons/142085
 def solution(n, k, enemy): #n 병사 k 무적권 enemy 라운드
     answer = 0
     # 무적권
     heap = []
+    temp = []
     for idx, e in enumerate(enemy):
         if idx <= k: # idx: 0, k: 3
             heappush(heap,  e) 
             continue
-        # answer: 4
-        # [4,2,4,5]
-        n -= heappop(heap)
-        if n >= e : #라운드 통과 () ()
-            n -= heappop(heap)
+        temp.append(e)
+    heap_n = heappop(heap)
+    if n >= heap_n:
+        n-=heap_n
+        answer += 1
+    else:
+        return k
+    for e in temp:
+        if n >= e:
+            n -= e
             answer+=1
+        else:
+            break
     return answer + k
-#                   #     #  #  
-print(solution(7,3,[4, 2, 4, 5, 3, 3, 1,10]))   # 5 
-print(solution(7,3,[8, 8, 8, 8, 3, 3, 1,10]))   # 3
+# ------------------------------------------------------
+# def solution(n, k, enemy): #n 병사 k 무적권 enemy 라운드
+#     answer = 0
+#     # 무적권
+#     heap = []
+#     for idx, e in enumerate(enemy):
+#         if idx <= k: # idx: 0, k: 3
+#             heappush(heap,  e) 
+#             continue
+#         # answer: 4
+#         # [4,2,4,5]
+#         n -= heappop(heap)
+#         if n >= e : #라운드 통과 () ()
+#             n -= heappop(heap)
+#             answer+=1
+#     return answer + k
+# #                   #     #  #  
+# print(solution(7,3,[4, 2, 4, 5, 3, 3, 1,10]))   # 5 
+# print(solution(7,3,[8, 8, 8, 8, 3, 3, 1,10]))   # 3
 
 # k랑 비교 
 
