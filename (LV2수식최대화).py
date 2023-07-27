325c8023f6aac13ce918167ce733fe215197cedd
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-07-27 10:32:15 +0900

    수식최대화

---
+++ (LV2수식최대화).py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/67257
+def solution(expression):
+    answer = 0
+    return answer
+print(solution("100-200*300-500+20")) #60420
+print(solution("50*6-3*2")) #300
