b2cab5fbb5a9b27191613fa1d16d6e02f99aa496
rhkrdudrb <78011285+rhkrdudrb@users.noreply.github.com>
2023-08-14 08:57:03 +0900

    20230814 효율성 넘어감

--- (LV2)주식가격.py
+++ (LV2)주식가격.py
@@ -7,16 +7,12 @@ def solution(prices):
         num = prices.popleft()
         cnt = 0
         for number in prices:
-            if num < number:
-                cnt +=1
-            else:
-                cnt = 1
+            cnt += 1
+            if num > number: 
+                break
         answer.append(cnt)        
-
-
     return answer
 
-# [1, 2, 3, 2, 3]	[4, 3, 1, 1, 0] 4 3 1 1 0
-# [1, 2, 3, 4, 5, 2]	[5, 4, 3, 2, 1, 0]
-
-# 떨어지면 무조건 1 안떨어지면 남은 길이 만큼 올름?
+print(solution([1, 2, 3, 2, 3])) #4 3 1 1 0
+print(solution([1, 2, 3, 4, 5, 2]))#	[5, 4, 3, 2, 1, 0]
+print(solution([2, 2, 3, 1, 5]))#	3, 2, 1, 1, 0
